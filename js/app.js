8a39a287f764315983def49817f6705ea1027f47
GflowSoftware <gflowSoftware@gmail.com>
2023-03-08 23:13:48 -0300

    fin clase

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var a =255;
+let b =120;
+
+let myDom = document; // contiene la estructura del html
+console.log(myDom);
+let myApp=document.getElementById("myApp");
+
+myApp.innerHTML = "Estoy modificando placeholder"
