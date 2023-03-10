1dd4bf3129888a457850e0e09a94912775b9d9c0
GflowSoftware <gflowSoftware@gmail.com>
2023-03-10 17:00:18 -0300

    clases y objetos

--- js/app.js
+++ js/app.js
@@ -12,17 +12,33 @@ const myCat = {
 console.log(myDog.name);
 console.log(myCat.name);
 
+/*
 class Mouse {
     constructor(name, age) {
         this.name = name;
         this.age = age;
 
     }
+}*/
+
+const myMouse = new Mouse("Perez",4);
+function Mouse(name, age) {
+    this.name=name;
+    this.age=age;
 }
+ const myMouse = new Mouse("Perez",4)
 
-const myMouse = new Mouse("Perez",4)
 
+ class Button {
+    constructor(parentID, text){
 
+    }
+    render() {
+
+    }
+ }
+
+ 
 /* clase 3
 console.log(myDom);
 let myApp=document.getElementById("myApp");
