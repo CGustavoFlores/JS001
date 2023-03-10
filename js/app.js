d78741187b003510de941411b5721cebc520ded4
GflowSoftware <gflowSoftware@gmail.com>
2023-03-10 16:40:38 -0300

    clases y objetos

--- js/app.js
+++ js/app.js
@@ -1,8 +1,32 @@
-var a =255;
-let b =120;
 
-let myDom = document; // contiene la estructura del html
+
+let myDom = document; // contiene la estructura del html, es un objeto global
+const myDog = {
+    name: "Firulais"
+};
+
+const myCat = {
+    "name":"Kity"
+};
+
+console.log(myDog.name);
+console.log(myCat.name);
+
+class Mouse {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+
+    }
+}
+
+const myMouse = new Mouse("Perez",4)
+
+
+/* clase 3
 console.log(myDom);
 let myApp=document.getElementById("myApp");
 
 myApp.innerHTML = "Estoy modificando placeholder"
+*/
+
